25e61edcdd5328bd8cb1e0a12607fa3d2d170335
NikolaiAsharin <112165231+NikolaiAsharin@users.noreply.github.com>
2023-06-20 18:37:30 +0400

    Add files via upload

---
+++ src/components/Copyright/Copyright.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Copyright.module.css";
+
+const Copyright = () => {
+  return (
+    <div className={styles.general}>
+      <span>Kanban board by Anton Parutov, 2023</span>
+    </div>
+  );
+};
+
+export default Copyright;
